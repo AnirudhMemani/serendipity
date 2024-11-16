85807da17996929c58b055872a780588bce4a731
AnirudhMemani <nemanianirudh@gmail.com>
2024-11-17 03:43:50 +0530

    fix: revert to v1 fixes

--- apps/backend/api/managers/RoomManager.ts
+++ apps/backend/api/managers/RoomManager.ts
@@ -15,6 +15,7 @@ export class RoomManager {
 
     createRoom(user1: User, user2: User) {
         const roomId = this.generate().toString();
+        console.log("Room ID:", roomId);
         this.rooms.set(roomId.toString(), {
             user1,
             user2,
@@ -29,24 +30,27 @@ export class RoomManager {
         });
     }
 
-    onOffer(roomId: string, sdp: string, senderSocketid: string) {
+    onOffer(roomId: string, sdp: string, senderSocketId: string) {
         const room = this.rooms.get(roomId);
+
         if (!room) {
             return;
         }
-        const receivingUser = room.user1.socket.id === senderSocketid ? room.user2 : room.user1;
+        const receivingUser = room.user1.socket.id === senderSocketId ? room.user2 : room.user1;
+
         receivingUser?.socket.emit("offer", {
             sdp,
             roomId,
         });
     }
 
-    onAnswer(roomId: string, sdp: string, senderSocketid: string) {
+    onAnswer(roomId: string, sdp: string, senderSocketId: string) {
         const room = this.rooms.get(roomId);
+
         if (!room) {
             return;
         }
-        const receivingUser = room.user1.socket.id === senderSocketid ? room.user2 : room.user1;
+        const receivingUser = room.user1.socket.id === senderSocketId ? room.user2 : room.user1;
 
         receivingUser?.socket.emit("answer", {
             sdp,
@@ -54,12 +58,14 @@ export class RoomManager {
         });
     }
 
-    onIceCandidates(roomId: string, senderSocketid: string, candidate: any, type: "sender" | "receiver") {
+    onIceCandidates(roomId: string, senderSocketId: string, candidate: any, type: "sender" | "receiver") {
         const room = this.rooms.get(roomId);
+
         if (!room) {
             return;
         }
-        const receivingUser = room.user1.socket.id === senderSocketid ? room.user2 : room.user1;
+        const receivingUser = room.user1.socket.id === senderSocketId ? room.user2 : room.user1;
+
         receivingUser.socket.emit("add-ice-candidate", { candidate, type });
     }
 
--- apps/backend/api/managers/UserManger.ts
+++ apps/backend/api/managers/UserManger.ts
@@ -29,31 +29,27 @@ export class UserManager {
     }
 
     removeUser(socketId: string) {
-        const user = this.users.find((x) => x.socket.id === socketId);
-
         this.users = this.users.filter((x) => x.socket.id !== socketId);
         this.queue = this.queue.filter((x) => x === socketId);
     }
 
     clearQueue() {
-        console.log("inside clear queues");
-        console.log(this.queue.length);
         if (this.queue.length < 2) {
             return;
         }
 
         const id1 = this.queue.pop();
         const id2 = this.queue.pop();
-        console.log("id is " + id1 + " " + id2);
         const user1 = this.users.find((x) => x.socket.id === id1);
         const user2 = this.users.find((x) => x.socket.id === id2);
 
         if (!user1 || !user2) {
             return;
         }
-        console.log("creating roonm");
 
-        const room = this.roomManager.createRoom(user1, user2);
+        console.log("creating room");
+
+        this.roomManager.createRoom(user1, user2);
         this.clearQueue();
     }
 
